d49d1a83fb525d0759918c467782af776ca55fa4
Hyun-woo <nikel4610@gmail.com>
2022-05-21 16:15:52 +0900

    learn and add sharedpreferences

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +14,12 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText editText_test;
+    EditText et_save;
     private Button btn_test;
     private EditText et_test;
     private String str;
     ImageView test;
+    String share = "file";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         et_test = findViewById(R.id.et_test);
+        et_save = (EditText)findViewById(R.id.et_save);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(share, 0);
+        String value = sharedPreferences.getString("amuname", "");
+        et_save.setText(value);
 
         editText_test = findViewById(R.id.editText_test);
         btn_test = findViewById(R.id.btn_test);
@@ -49,6 +57,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(share, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String value = et_save.getText().toString();
+        editor.putString("amuname", value); // 밸류값 이름 아무거나 가능
+        editor.commit(); // 저장
     }
 }
 
