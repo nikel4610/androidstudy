a5ebb107f0efd6e5e2989a35588bd2ecd39ff4c7
Hyun-woo <nikel4610@gmail.com>
2022-05-21 15:29:11 +0900

    list data set

--- app/src/main/java/com/example/myapplication/SubActivity.java
+++ app/src/main/java/com/example/myapplication/SubActivity.java
@@ -4,18 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SubActivity extends AppCompatActivity {
 
     private TextView tv_sub;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
+        list = (ListView)findViewById(R.id.list);
         tv_sub = findViewById(R.id.tv_sub);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("넣고싶은 데이터"); // 여러개도 상관 없음 -> 리스트형으로 데이터 전달
+        data.add("이것도 넣어봐야지");
+        adapter.notifyDataSetChanged();
 
         Intent intent = getIntent();
         String str = intent.getStringExtra("str"); // get str from MainActivity
